LeafOS-Project/android_frameworks_base

SystemUI: keyguard: preview: Catch InterruptedException
04-02 17:30:29.011 10012 10040 E AndroidRuntime: java.lang.InterruptedException
04-02 17:30:29.011 10012 10040 E AndroidRuntime: 	at kotlinx.coroutines.BuildersKt.runBlocking(Unknown Source:209)
04-02 17:30:29.011 10012 10040 E AndroidRuntime: 	at com.android.systemui.keyguard.ui.preview.KeyguardRemotePreviewManager.destroyObserver(KeyguardRemotePreviewManager.kt:26)
04-02 17:30:29.011 10012 10040 E AndroidRuntime: 	at com.android.systemui.keyguard.ui.preview.KeyguardRemotePreviewManager$preview$2.invoke(KeyguardRemotePreviewManager.kt:6)
04-02 17:30:29.011 10012 10040 E AndroidRuntime: 	at com.android.systemui.keyguard.ui.preview.KeyguardRemotePreviewManager$PreviewLifecycleObserver.handleMessage(KeyguardRemotePreviewManager.kt:31)
04-02 17:30:29.011 10012 10040 E AndroidRuntime: 	at android.os.Handler.dispatchMessage(Handler.java:102)
04-02 17:30:29.011 10012 10040 E AndroidRuntime: 	at android.os.Looper.loopOnce(Looper.java:201)
04-02 17:30:29.011 10012 10040 E AndroidRuntime: 	at android.os.Looper.loop(Looper.java:288)
04-02 17:30:29.011 10012 10040 E AndroidRuntime: 	at android.os.HandlerThread.run(HandlerThread.java:67)

Change-Id: Iecda10e2f405b2a99ca9bb33819156d8823f5ea2

packages/SystemUI/src/com/android/systemui/keyguard/ui/preview/KeyguardRemotePreviewManager.kt
@@ -125,7 +125,11 @@ constructor(
             isDestroyed = true
             val hostToken = renderer.hostToken
             hostToken?.unlinkToDeath(this, 0)
-            runBlocking(mainDispatcher) { renderer.destroy() }
+            try {
+                runBlocking(mainDispatcher) { renderer.destroy() }
+            } catch (e: Exception) {
+                Log.w(TAG, "Interrupted while destroying observer", e)
+            }
             return hostToken
         }
     }

all-backupss/_frameworks_base

packages/SystemUI/src/com/android/systemui/keyguard/ui/preview/KeyguardRemotePreviewManager.kt
@@ -125,7 +125,11 @@ constructor(
             isDestroyed = true
             val hostToken = renderer.hostToken
             hostToken?.unlinkToDeath(this, 0)
-            runBlocking(mainDispatcher) { renderer.destroy() }
+            try {
+                runBlocking(mainDispatcher) { renderer.destroy() }
+            } catch (e: InterruptedException) {
+                Log.w(TAG, "Interrupted while destroying observer", e)
+            }
             return hostToken
         }
     }